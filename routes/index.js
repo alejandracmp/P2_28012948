a180fa13a741b63415fa01b95ea0303c6db0de79
isabelasimancas <isabelasg2001@gmail.com>
2022-06-24 21:22:39 -0400

    commit1

--- routes/index.js
+++ routes/index.js
@@ -23,8 +23,8 @@ router.get('/login', function(req, res,) {
 });
 
 
-const testu= process.env.MYUS;
-const testp = process.env.MYPAS;
+const testu=process.env.MYUS;
+const testp=process.env.MYPAS;
 
 router.post('/login', (req,res) => {
 	 var usuario=req.body.usuario;
